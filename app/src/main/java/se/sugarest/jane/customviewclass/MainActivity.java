a3a6f6c8c7f10347bbb4ca5515cda5b87f3178ce
jinyanliu <jane.jinyan.liu@gmail.com>
2017-08-26 17:23:40 +0200

    Press the button and the custom view can change back and forth.

--- app/src/main/java/se/sugarest/jane/customviewclass/MainActivity.java
+++ app/src/main/java/se/sugarest/jane/customviewclass/MainActivity.java
@@ -3,9 +3,11 @@ package se.sugarest.jane.customviewclass;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     private CustomView mCustomView;
 
@@ -18,9 +20,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void btnPressed(View view) {
-        // update the View
-        mCustomView.setCircleColor(Color.GREEN);
-        mCustomView.setLabelColor(Color.MAGENTA);
-        mCustomView.setCircleText("hehe");
+        Log.i(TAG, "Text in the circle is: " + mCustomView.getCircleText());
+        if (mCustomView.getCircleText() == "Hello") {
+            mCustomView.setCircleColor(Color.GREEN);
+            mCustomView.setLabelColor(Color.MAGENTA);
+            mCustomView.setCircleText("hehe");
+        } else {
+            mCustomView.setCircleColor(Color.parseColor("#ff0099"));
+            mCustomView.setLabelColor(Color.parseColor("#ffff66"));
+            mCustomView.setCircleText("Hello");
+        }
     }
 }
